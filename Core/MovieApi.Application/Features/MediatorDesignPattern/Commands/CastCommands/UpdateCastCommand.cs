b4ef01fc71e2b07ad3f87adba8064222d70a8357
Gülistan Aksoy <gulistanaksoy87@gmail.com>
2025-08-12 00:15:23 +0300

    MediatorDesignPattern yapısı oluşturuldu

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Commands/CastCommands/UpdateCastCommand.cs
@@ -0,0 +1,16 @@
+using MediatR;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Commands.CastCommands
+{
+    public class UpdateCastCommand : IRequest
+    {
+        public int CastId { get; set; }  // Primary key
+        public string Title { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+        public string? Overview { get; set; }
+        public string? Biography { get; set; }
+
+    }
+}
